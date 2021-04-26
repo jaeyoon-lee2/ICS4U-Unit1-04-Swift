30f57ac87ba5b8958c31b0f466bb84d3306f8a44
jaeyoonlee2 <>
2021-04-26 13:57:28 +0000

    Create HeatingTime.swift

---
+++ HeatingTime.swift
@@ -0,0 +1,50 @@
+/*
+This program gets the type of food and number of food,
+calculates and display the time to reheat in the microwave.
+author  Jay Lee
+version 1.0
+since   2020-04-26
+*/
+import Foundation
+
+let foodTypeList = ["PIZZA", "SUB", "SOUP"]
+let individualTime: Float
+
+// String Input
+print("Are you heating a sub, pizza, or soup? ", terminator: "")
+let foodType = String(readLine()!)
+
+// Check user's food is sub, pizza or soup
+if (foodTypeList.contains(foodType.uppercased())) {
+
+  if (foodTypeList.firstIndex(of: foodType.uppercased()) == 0) {
+    individualTime = 0.75  // Pizza: 45 seconds
+  }
+  else if (foodTypeList.firstIndex(of: foodType.uppercased()) == 1) {
+    individualTime = 1.0  // Sub : 1 minute
+  }
+  else {
+    individualTime = 1.75  // Soup: 1 minute 45 seconds
+  }
+  // Integer Input
+  print("How many \(foodType)(s) are you heating (max 3 and they must all be "
+      + "the same items)? ", terminator: "")
+  let foodNumber = Int(readLine()!)
+
+  if (Int(foodNumber!) <= 3) {
+    // Process
+    let cookTime = (individualTime / Float(foodNumber!)) 
+                    * pow(3.0, (Float(foodNumber!) - 1))
+    // convert float minutes to minutes and seconds
+    let cookTimeMin = floor(cookTime)
+    let cookTimeSec = (cookTime - cookTimeMin) * 60
+
+    // Output
+    print("The total cook time is \(Int(cookTimeMin)) min \(cookTimeSec) sec.")
+  } else {
+    print("Max number of food is 3.")
+  }
+} else {
+  print("Invalid input.")
+}
+print("\nDone.")
